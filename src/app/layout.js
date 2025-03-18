e30deecf725672e60c6e23d068f9b9a523854fc2
Munkhtur <munkhtur@nashatech.com>
2025-03-18 16:26:39 +0800

    add speed-insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "Create Resourse, Chance, Strategy",
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <>
       {children}
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
